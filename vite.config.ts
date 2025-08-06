6e5b0e8181fa47a3bd82fc94cbaf709e4562493b
sunniekapar <kaparsunnie@gmail.com>
2025-08-05 23:59:50 -0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+
+export default defineConfig({
+  server: {
+    port: 4000,
+  },
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart({ customViteReactPlugin: true }),
+    viteReact(),
+  ],
+})
